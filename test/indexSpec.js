7785890164151a235e5a04082fae36e6b68fe046
Peter Marshall <p.s.marshall0@gmail.com>
2017-01-05 23:02:43 +0100

    Set up testing with chai-http

--- test/indexSpec.js
+++ test/indexSpec.js
@@ -1,10 +1,19 @@
 var index = require("../index.js");
-var expect = require("chai").expect;
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var should = chai.should();
+
+chai.use(chaiHttp);
 
 describe("Index", function() {
   describe("setup", function() {
-  	it('should setup correctly', function() {
-  		
-  	})
+    it("returns status 200", function(done) {
+      chai.request('http://localhost:3000')
+        .get('/')
+        .end(function(err, res) {
+          res.should.have.status(200);
+          done();
+        });
+    });
   })
 });
