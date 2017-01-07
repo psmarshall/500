57434ebb40e472fd4f4f534605c51057f5a53429
Peter Marshall <p.s.marshall0@gmail.com>
2017-01-07 20:12:25 +0100

    deck -> pack

--- public/game/game.js
+++ public/game/game.js
@@ -4,16 +4,16 @@ var Card = require('./card');
 
 module.exports = class Game {
   standardDeck() {
-    var deck = [];
+    var pack = [];
     for (let suit of Card.SUIT_TYPES) {
       for (let number of Card.NUMBER_TYPES) {
         var imagePath = number + '_of_' + suit;
         // Use full face card images.
         if (Card.isFaceCard(number)) imagePath += '2';
         var card = new Card(suit, number, imagePath);
-        deck.push(card);
+        pack.push(card);
       }
     }
-    return deck;
+    return pack;
   }
 }
